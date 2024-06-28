d412bd9ca328cdb989a020698126d48724b61f79
fps_netodf <fps_netodf@hotmail.com>
2024-06-28 16:28:46 -0300

    antes de configurar as messages do django

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -11,7 +14,31 @@ def novo(request):
 
 
 def cadastro(request):
-    return render(request, "usuarios/cadastro.html")
+    
+    if request.method == 'GET':
+        return render(request, "usuarios/cadastro.html")
+    elif request.method == 'POST':
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha') 
+        if not senha == confirmar_senha:
+            message = "Sua senha não conferi"
+            return render(request, "usuarios/cadastro.html", {"message": message})
+        if len(senha) < 6:
+            message = "a senha dever ter pelo menos 6 digitos"
+            return render(request, "usuarios/cadastro.html", {"message": message}) 
+        
+        user = User.objects.create_user(
+            first_name = primeiro_nome, 
+            last_name = ultimo_nome, 
+            username = username, 
+            email = email, 
+            password = senha
+        )    
+        return HttpResponse(f"passou")
 
 
 def login(request):
