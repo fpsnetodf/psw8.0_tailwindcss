9dbb5411aa28526e933b7c8c6ca4216aee17cd9f
fps_netodf <fps_netodf@hotmail.com>
2024-07-29 08:18:24 -0300

    psw8 cadastro

--- vitalab/views.py
+++ vitalab/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from usuarios.models import PedidosExames, TiposExames
+from datetime import datetime
 
 # Create your views here.
 # def solicitar_exames(request):
@@ -11,12 +13,20 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def solicitar_exames(request): 
-    if request.method == "GET":   
-        return render(request, 'vitalab/solicitar_exames.html')  
+    if request.method == "GET": 
+        tipos_exames = TiposExames.objects.all()  
+        return render(request, 'vitalab/solicitar_exames.html', {"tipos_exames": tipos_exames})  
     
     
+def fechar_pedido(request):
+    exames_id = request.POST.getlist('exames')
 
-
-
+    pedidos_exame = PedidosExames(
+        usuario=request.user, 
+        data=datetime.now()
+        
+    )
+    pedidos_exame.save()
+    return HttpResponse('estou aqui')
 
       
